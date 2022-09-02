621c04d4aaf811ce5ad99fd734aa6f643b5f76b6
David <cruz-azul99@hotmail.com>
2022-09-02 16:46:23 -0500

    Polls routes created

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+# Esta variable es usada internamente por Django y es recomendada para poder usar URLs dinámicas en los templates HTML
+app_name = "polls"
+
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  # De esta manera se envía un parámetro en la URL
+  path('<int:question_id>/', views.detail, name='detail'),
+  path('<int:question_id>/results', views.results, name='results'),
+  path('<int:question_id>/votes', views.vote, name='vote'),
 ]
